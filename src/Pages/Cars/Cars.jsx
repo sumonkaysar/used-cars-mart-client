6a545161c2de095c58749e961f41d7aba5319e29
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2022-11-25 20:44:45 +0600

    added car booking function

--- src/Pages/Cars/Cars.jsx
+++ src/Pages/Cars/Cars.jsx
@@ -1,27 +1,39 @@
-import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
 import CarDetails from "./CarDetails/CarDetails";
 import CarsBookingModal from "./CarsBookingModal/CarsBookingModal";
 
 const Cars = () => {
-  const cars = useLoaderData();
-  const [bookCar, setBookCar] = useState({});
+  const { user } = useContext(AuthContext);
+  const [bookCar, setBookCar] = useState(null);
+  const { id } = useParams();
+  const { data: cars = [], refetch } = useQuery({
+    queryKey: ['cars', id, user?.email],
+    queryFn: () => fetch(`http://localhost:5000/categories/${id}?email=${user?.email}`).then(res => res.json())
+  });
 
   return (
     <div>
-      <h2 className="text-3xl font-semibold">{}</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-5">
-        {
-          cars.map(car => <CarDetails
-            key={car._id}
-            setBookCar={setBookCar}
-            car={car}
-          />)
-        }
-      </div>
+      <h2 className="text-3xl font-semibold">{ }</h2>
+      {
+        cars.length > 0 ?
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-5">
+            {
+              cars.map(car => <CarDetails
+                key={car._id}
+                setBookCar={setBookCar}
+                car={car}
+              />)
+            }
+          </div> : <h2 className="text-4xl text-center font-bold">No car found</h2>
+      }
       {
         bookCar && <CarsBookingModal
           bookCar={bookCar}
+          setBookCar={setBookCar}
+          refetch={refetch}
         />
       }
     </div>
--- src/Pages/Cars/CarsBookingModal/CarsBookingModal.jsx
+++ src/Pages/Cars/CarsBookingModal/CarsBookingModal.jsx
@@ -1,22 +1,132 @@
 import { useContext } from "react";
 import { AuthContext } from "../../../contexts/AuthProvider";
+import { toast } from "react-toastify";
 
-const CarsBookingModal = ({bookCar}) => {
-  const {user} = useContext(AuthContext);
-  const {name, resalePrice} = bookCar;
+const CarsBookingModal = ({ bookCar, setBookCar, refetch }) => {
+  const { user } = useContext(AuthContext);
+  const { _id, name, resalePrice } = bookCar;
+
+  const handleBooking = e => {
+    e.preventDefault();
+    const form = e.target;
+    const buyerName = form.buyerName.value;
+    const buyerEmail = form.buyerEmail.value;
+    const carId = form.carId.value;
+    const carName = form.carName.value;
+    const price = Number(form.price.value);
+    const phone = form.phone.value;
+    const meetingLocation = form.meetingLocation.value;
+
+    const booking = {
+      buyerName,
+      buyerEmail,
+      carId,
+      carName,
+      price,
+      phone,
+      meetingLocation
+    }
+
+    fetch('http://localhost:5000/bookCar', {
+      method: 'POST',
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(booking)
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.acknowledged) {
+          toast.success('Booked Successfully');
+          setBookCar(null);
+          refetch();
+        } else {
+          toast.error(data.message);
+        }
+      }).catch(err => console.error(err))
+  }
 
   return (
     <div>
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
       <div className="modal">
         <div className="modal-box">
-          <input defaultValue={user?.displayName} type="text" className="input input-bordered w-full mb-2" disabled />
-          <input defaultValue={user?.email} type="email" className="input input-bordered w-full mb-2" disabled />
-          <input defaultValue={name} type="text" className="input input-bordered w-full mb-2" disabled />
-          <input value={`$${resalePrice}`} onChange={() => {}} type="text" className="input input-bordered w-full mb-2" disabled />
-          <div className="modal-action">
-            <label htmlFor="booking-modal" className="btn">Yay!</label>
-          </div>
+          <form onSubmit={handleBooking}>
+            <input
+              name="carId"
+              type="text"
+              defaultValue={_id}
+              hidden
+            />
+            <div className="form-control w-full  mb-2">
+              <label htmlFor="buyerName" className="label">Name:</label>
+              <input
+                id="buyerName"
+                name="buyerName"
+                defaultValue={user?.displayName}
+                type="text"
+                className="input input-bordered w-full"
+                disabled
+              />
+            </div>
+            <div className="form-control w-full  mb-2">
+              <label htmlFor="buyerEmail" className="label">Email:</label>
+              <input
+                id="buyerEmail"
+                name="buyerEmail"
+                defaultValue={user?.email}
+                type="email"
+                className="input input-bordered w-full"
+                disabled
+              />
+            </div>
+            <div className="form-control w-full  mb-2">
+              <label htmlFor="carName" className="label">Car Name:</label>
+              <input
+                id="carName"
+                name="carName"
+                defaultValue={name}
+                type="text"
+                className="input input-bordered w-full"
+                disabled
+              />
+            </div>
+            <div className="form-control w-full  mb-2">
+              <label htmlFor="price" className="label">Price (USD):</label>
+              <input
+                id="price"
+                name="price"
+                defaultValue={resalePrice}
+                type="text"
+                className="input input-bordered w-full"
+                disabled
+              />
+            </div>
+            <div className="form-control w-full  mb-2">
+              <label htmlFor="phone" className="label">Phone Number:</label>
+              <input
+                id="phone"
+                name="phone"
+                type="text"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+            <div className="form-control w-full  mb-2">
+              <label htmlFor="meetingLocation" className="label">Meeting Location:</label>
+              <input
+                id="meetingLocation"
+                name="meetingLocation"
+                type="text"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+            <div className="">
+              <button className="btn btn-primary" type="submit">Book</button>
+              <label htmlFor="booking-modal" className="btn btn-outline ml-3">Cancel</label>
+            </div>
+          </form>
         </div>
       </div>
     </div>
